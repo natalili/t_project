b26bef4ad32fafb3fe3b5abef8ceff1254aa5a53
natalili <natali.li@list.ru>
2012-06-25 14:15:11 +0300

    User model and controller with new page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+#      sign_in @user
+      flash[:success] = "Welcome to the Test Application!"
+#      redirect_to #root_path
+    else
+      @user.password = ''
+      @user.password_confirmation = ''
+      render 'new'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+#    redirect_to #root_path
+#    render 'new'
+  end
+end
